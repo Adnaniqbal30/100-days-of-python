4a59d30cfb8c043cb60a66460bee9556dd92968d
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-02-03 23:17:51 +0530

    Day 1-3 complete: Python fundamentals - Variables, Data Types, Control Flow

---
+++ Day 1/task/task.py
@@ -0,0 +1,3 @@
+city=input("Enter Your City\n")
+pet=input("Enter Your Pet Name\n")
+print("Your band name is " + city +" "+ pet)
---
+++ Day 3/If Else/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height>=180:
+    print("You are allowed to ride")
+else:
+    print("You are not allowed")
