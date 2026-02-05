e769d66c612ed26e600ccef7c4338da090d332ee
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-02-05 22:22:46 +0530

    Day 3: Days 8-9 complete, Day 10 midway - Caesar Cipher, Blind Auction, Calculator WIP

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,7 @@
+def format_name(f_name,l_name):
+    # f_name=input("Enter first name: ")
+    # l_name=input("Enter last name: ")
+    formatted_f_name=f_name.title()
+    formatted_l_name=l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+print(format_name("adnan","iqbal"))
--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,20 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+import art
+print(art.logo)
+auction={}
+while True:
+     name = input("What is your name: ")
+     bid_price = input("What's your bid: ")
 
+     auction[name]=bid_price
+     print("\n"*20)
+     new_bid=input("Are there any other bidders, yes or no.").lower()
 
+     if new_bid=="no":
+         break
+
+highest_value_key=max(auction, key=auction.get)
+highest_value=max(auction.values())
+print(f"The Winner is {highest_value_key} with a bid of {highest_value}.")
