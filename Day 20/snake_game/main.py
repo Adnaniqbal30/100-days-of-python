3b040a64c857535d7a8845d6052ba632eff0cbc3
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-02-21 22:10:17 +0530

    Day 20 WIP

---
+++ Day 20/snake_game/main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle,Screen
+import time
+from snake import Snake
+screen=Screen()
+screen.tracer(0)
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+snake=Snake()
+
+game_is_on=True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ Day 20/snake_game/snake.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+class Snake():
+    def __init__(self):
+        starting_posn=[(0, 0),(-20, 0),(-40, 0)]
+        segments=[]
+        for posn in starting_posn:
+            new_segments=Turtle()
+            new_segments.color("white")
+            new_segments.shape("square")
+            new_segments.penup()
+            new_segments.goto(posn)
+            segments.append(new_segments)
+
+        for seg_num in range(len(segments) - 1, 0, -1):
+            new_x = segments[seg_num - 1].xcor()
+            new_y = segments[seg_num - 1].ycor()
+            segments[seg_num].goto(new_x, new_y)
+
+    def move(self):
+        segm
