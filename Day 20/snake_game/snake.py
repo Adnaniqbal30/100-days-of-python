07e8a21feaba1625f32c4438e28b82b36620bdc4
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-02-22 22:59:19 +0530

    Day 20 WIP

--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -15,6 +15,3 @@ class Snake():
             new_x = segments[seg_num - 1].xcor()
             new_y = segments[seg_num - 1].ycor()
             segments[seg_num].goto(new_x, new_y)
-
-    def move(self):
-        segm
