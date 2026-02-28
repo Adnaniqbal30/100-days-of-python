d9698861c6c0a9e1cb0206b397cbbc04b2eb2691
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-02-28 18:47:37 +0530

    Commit

--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -16,4 +16,8 @@ class Snake:
             new_y = segments[seg_num - 1].ycor()
             segments[seg_num].goto(new_x, new_y)
 
+        for seg_num in range(len(segments) - 1, 0, -1):
+            new_x = segments[seg_num - 1].xcor()
+            new_y = segments[seg_num - 1].ycor()
+            segments[seg_num].goto(new_x, new_y)
 
