ca9ca63370a52266be81fc43c9394319f19575c6
Adnaniqbal30 <adnan30iqbal@gmail.com>
2026-03-07 22:46:16 +0530

    commit

--- Day 20/snake_game/snake.py
+++ Day 20/snake_game/snake.py
@@ -20,4 +20,4 @@ class Snake:
             new_x = segments[seg_num - 1].xcor()
             new_y = segments[seg_num - 1].ycor()
             segments[seg_num].goto(new_x, new_y)
-        dfddvvvvv
+
